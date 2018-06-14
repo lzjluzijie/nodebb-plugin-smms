84281401b23dcacc3b222e88b0e555a9ffaf41c1
halulu <lzjluzijie@gmail.com>
2018-06-14 21:08:50 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var request = require('request');
+var fs = require('fs');
+
+var plugin = {};
+
+plugin.upload = function(data, callback) {
+	var image = data.image;
+
+	if (!image) {
+		return callback(new Error('invalid image'));
+	}
+
+	var type = image.url ? 'url': 'file';
+	if (type === 'file' && !image.path) {
+		return callback(new Error('invalid image path'));
+	}
+
+	var formDataImage;
+	if (type === 'file') {
+		formDataImage = fs.createReadStream(image.path);
+		formDataImage.on('error',
+		function(err) {
+			done(err);
+		});
+	} else if (type === 'url') {
+		formDataImage = image.url;
+	} else {
+		return callback(new Error('unknown-type'));
+	}
+
+	var options = {
+		url: 'https://sm.ms/api/upload',
+		headers: {
+			'User-Agent': 'request'
+		},
+		formData: {
+			smfile: formDataImage
+		}
+	};
+
+	request.post(options, function optionalCallback(err, httpResponse, body) {
+		if (err) {
+			return console.error('upload failed:', err);
+		}
+		var resp;
+		try {
+			resp = JSON.parse(body);
+		} catch(err) {
+			return console.error('parse json failed:', err);
+		}
+		
+		console.log('Upload successful! Server responded with:', body);
+		return callback(null, {
+			name: image.name,
+			url: resp.data.url
+		});
+	});
+
+};
+
+module.exports = plugin;
